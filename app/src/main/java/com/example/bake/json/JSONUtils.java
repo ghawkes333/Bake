27cd59b80cb95687e37bc9720ede214184050b32
Gabriella Hawkes <appsalothelp@gmail.com>
2020-05-27 16:43:18 -0500

    feat: Add ability to parse JSON

---
+++ app/src/main/java/com/example/bake/json/JSONUtils.java
@@ -0,0 +1,86 @@
+package com.example.bake.json;
+
+import android.content.Context;
+
+import com.example.bake.R;
+import com.example.bake.objects.Recipe;
+import com.example.bake.objects.Step;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONUtils {
+
+    public static List<Recipe> parseJSON(JSONArray array, Context context){
+        List<Recipe> recipes = new ArrayList<>();
+        try {
+            for(int i = 0; i < array.length(); i++){
+                JSONObject recipeJSON = array.getJSONObject(i);
+                int id = recipeJSON.getInt(RecipeValues.RESPONSE_META_ID);
+                String name = recipeJSON.getString(RecipeValues.RESPONSE_META_NAME);
+                List<String> ingredients = parseIngredients(recipeJSON.getJSONArray(RecipeValues.RESPONSE_META_INGREDIENTS), context);
+                List<Step> steps = parseSteps(recipeJSON.getJSONArray(RecipeValues.RESPONSE_META_STEPS));
+                Recipe recipe = new Recipe(id, name, steps, ingredients);
+                recipes.add(recipe);
+            }
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        return recipes;
+    }
+
+    private static List<Step> parseSteps(JSONArray stepsJSON) throws JSONException {
+        List<Step> stepsList = new ArrayList<>();
+        for(int i = 0; i < stepsJSON.length(); i++){
+            JSONObject stepJSON = stepsJSON.getJSONObject(i);
+            int id = stepJSON.getInt(RecipeValues.RESPONSE_STEPS_ID);
+            String shortDescription = stepJSON.getString(RecipeValues.RESPONSE_STEPS_SHORT_DESCRIPTION);
+            String description = stepJSON.getString(RecipeValues.RESPONSE_STEPS_LONG_DESCRIPTION);
+            String videoURL = stepJSON.getString(RecipeValues.RESPONSE_STEPS_VIDEO_URL);
+            String imageURL = stepJSON.getString(RecipeValues.RESPONSE_STEPS_IMAGE_URL);
+
+
+            Step step = new Step(id, shortDescription, description, videoURL, imageURL);
+            stepsList.add(step);
+        }
+
+        return stepsList;
+    }
+
+    /*
+    * @param ingredientsJSON is the JSONArray of ingredients returned from the network call
+    * */
+    private static List<String> parseIngredients(JSONArray ingredientsJSON, Context context) throws JSONException {
+        List<String> ingredientsList = new ArrayList<>();
+        for(int i = 0; i < ingredientsJSON.length(); i++){
+            JSONObject ingredient = ingredientsJSON.getJSONObject(i);
+            String quantity = ingredient.getString(RecipeValues.RESPONSE_INGREDIENTS_QUANTITY);
+            String measure = ingredient.getString(RecipeValues.RESPONSE_INGREDIENTS_MEASURE);
+            String ingredientName = ingredient.getString(RecipeValues.RESPONSE_INGREDIENTS_INGREDIENT);
+
+            String unit = measuresReadable(measure, context);
+            String of = context.getString(R.string.of);
+            String space = context.getString(R.string.space);
+
+            String ingredientItem = quantity + space + unit + space + of + space + ingredientName;
+            ingredientsList.add(ingredientItem);
+        }
+
+        return ingredientsList;
+    }
+
+    /*
+    * Converts a JSON-response unit to a human-readable string
+    * */
+    private static String measuresReadable(String measure, Context context){
+        String string = context.getString(R.string.string);
+        int resId = context.getResources().getIdentifier(measure, string, context.getPackageName());
+        if(resId > 0){
+            return context.getString(resId);
+        } else return measure;
+    }
+}
