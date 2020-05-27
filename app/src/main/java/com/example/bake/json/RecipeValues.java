02ee69629257831742be5049024fcea71247feee
Gabriella Hawkes <appsalothelp@gmail.com>
2020-05-27 16:41:59 -0500

    fix: Add steps string

---
+++ app/src/main/java/com/example/bake/json/RecipeValues.java
@@ -0,0 +1,18 @@
+package com.example.bake.json;
+
+public class RecipeValues {
+    public static final String RESPONSE_META_ID = "id";
+    public static final String RESPONSE_META_NAME = "name";
+    public static final String RESPONSE_META_INGREDIENTS = "ingredients";
+    public static final String RESPONSE_META_STEPS = "steps";
+
+    public static final String RESPONSE_INGREDIENTS_QUANTITY = "quantity";
+    public static final String RESPONSE_INGREDIENTS_MEASURE = "measure";
+    public static final String RESPONSE_INGREDIENTS_INGREDIENT = "ingredient";
+
+    public static final String RESPONSE_STEPS_ID = "id";
+    public static final String RESPONSE_STEPS_SHORT_DESCRIPTION = "shortDescription";
+    public static final String RESPONSE_STEPS_LONG_DESCRIPTION = "description";
+    public static final String RESPONSE_STEPS_VIDEO_URL = "videoURL";
+    public static final String RESPONSE_STEPS_IMAGE_URL = "thumbnailURL";
+}
